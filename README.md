b8efd472ffcbeb7dedb03de32143879e6d798899
Geisabitt <ageisislane@gmail.com>
2023-01-02 18:57:07 -0300

    desenvolvimento LandingPage Jornada da protagonista

---
+++ README.md
@@ -0,0 +1,15 @@
+### Desenvolvimento do Site Jornada da Protagonista
+
+## Projeto mobile first
+
+- [x] Navbar
+- [x] Call to action com Foto
+- [x] Informação Data e Local
+- [x] Botões de Call to action
+- [x] Sobre a proficional
+- [x] Call to action Seja Protagonista
+- [x] Sobre o Evento
+- [x] Botão Garanta sua Vaga
+- [x] Formulário de Contado html
+- [x] JS Formulário de contato envio por whatsapp
+- [ ] Scroll animado
---
+++ js/envioMensagem.js
@@ -0,0 +1,37 @@
+function enviarMensagem() {
+  var nome = document.querySelector("#nome").value;
+  nome = window.encodeURIComponent(nome);
+
+  var cidadeBairro = document.querySelector("#cidadeBairro").value;
+  cidadeBairro = window.encodeURIComponent(cidadeBairro);
+
+  var celular = document.querySelector("#celular").value;
+  celular = celular.replace(/\D/g, "");
+
+  var mensagem = document.querySelector("#mensagem").value;
+  mensagem = window.encodeURIComponent(mensagem);
+  if (nome === "" || nome.length < 5) {
+    alert("É obrigatório informar o seu Nome");
+    nome.focus();
+  } else {
+    let urlApi = "https://api.whatsapp.com/send?phone=5521982708329&text=";
+
+    window.open(
+      urlApi +
+        "Nome:%20" +
+        nome +
+        "%0d%0a" +
+        "Cidade:%20" +
+        cidadeBairro +
+        "%0d%0a" +
+        "Telefone:%20" +
+        celular +
+        "%0d%0a" +
+        "Mensagem:%20" +
+        mensagem,
+      "_blank"
+    );
+  }
+
+  //Obs.. use "_system", no lugar de blank, caso você esteja usando Phonegap / Cordova / Ionic ou qualquer um baseado em webview;
+}
